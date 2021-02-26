72088473445371ef55e154dc0b85aa43c0870be9
Jon-McMonagle <mcmonagl@ualberta.ca>
2021-02-25 22:23:24 -0700

    updated requirements file, feb 25, 2021

--- requirements.sh
+++ requirements.sh
@@ -5,10 +5,18 @@ sudo apt-get install libopenblas-dev liblapack-dev
 sudo apt-get install libx11-dev libgtk-3-dev
 sudo apt-get install python python-dev python3-pip
 sudo pip3 install numpy
-sudo pip3 install dlib
+sudo pip3 install opencv-python
+sudo apt-get install libcblas-dev
+sudo apt-get install libhdf5-dev
+sudo apt-get install libhdf5-seral-dev
+sudo apt-get install libatlas-base-dev
+sudo apt-get install libjasper-dev
+sudo apt-get install libqtgui4
+sudo apt-get install libqt4-test
+sudo pip install dlib
 sudo pip3 install face_recognition
 sudo pip3 install imutils
 sudo pip3 install matplotlib
-sudo pip3 install opencv-python
+#sudo pip3 install opencv-python
 sudo apt-get install xrdp
 #sudo apt install git
